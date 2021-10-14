cd32a5234769bc7811fbd19009b6e5896d294cf1
Piet Cordemans <cordemans.piet@gmail.com>
2021-10-14 10:37:07 +0200

    refactors sum of even Lucas numbers

--- Exercises/SumOddLucasRow.py
+++ Exercises/SumOddLucasRow.py
@@ -1,15 +1,24 @@
-current = 1
-previous = 2
-sum = 2
 
-while current < 4000000:
-    nextNumber = previous + current
 
-    if nextNumber % 2 == 0:
-        sum += nextNumber
+def luc(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return luc(n-1) + luc(n-2)
 
-    previous = current
-    current = nextNumber
-    
+def sumOfEvenLucasNumbers(n):
+    current = 1
+    sum = 2
+    while current < n:
+        nextNumber = luc(current)
 
-print(sum)
+        if nextNumber % 2 == 0:
+            sum += nextNumber
+        
+        current += 1
+    return sum
+
+
+print(sumOfEvenLucasNumbers(4))
